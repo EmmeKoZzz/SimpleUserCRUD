2b9f7c64bb7864298631d73af29ab2b30d35e9bd
EmmeKoZzz <119811212+EmmeKoZzz@users.noreply.github.com>
2023-03-28 11:58:12 -0400

    old repo migration

---
+++ src/components/TasksComponent/AddTasks/Menu/Menu.tsx
@@ -0,0 +1,103 @@
+import { useContext, useEffect, useRef } from 'react';
+import {
+	calendarIcon,
+	discIcon,
+	loaderIcon,
+	maximizeIcon,
+	unlockIcon,
+} from '../../../../assets/Icons';
+import { AddTaskContext } from '../context/AddTasksContext';
+import TaskMenuButton from './ButtonTaskList';
+
+function TaskMenu() {
+	const todayButtonRef = useRef<HTMLButtonElement>(null);
+	const publicButtonRef = useRef<HTMLButtonElement>(null);
+	const normalButtonRef = useRef<HTMLButtonElement>(null);
+	const estimationButtonRef = useRef<HTMLButtonElement>(null);
+	const buttonOkRef = useRef<HTMLButtonElement>(null);
+
+	const [Text, SetText] = useContext(AddTaskContext).InputText;
+	const SetMenu = useContext(AddTaskContext).MenuDisplay[1];
+
+	//* ButtonAllower
+	useEffect(() => {
+		const Buttons = [
+			todayButtonRef,
+			publicButtonRef,
+			normalButtonRef,
+			estimationButtonRef,
+		];
+
+		const buttonOk =
+			buttonOkRef.current === null ? new HTMLElement() : buttonOkRef.current;
+
+		const buttonState = (buttons: typeof Buttons, state: boolean) => {
+			buttons.map((button) =>
+				state
+					? button.current?.classList.add('disabled-button')
+					: button.current?.classList.remove('disabled-button')
+			);
+		};
+
+		if (Text === '') {
+			buttonOk.innerHTML = 'Ok';
+			buttonState(Buttons, true);
+		} else {
+			buttonOk.innerHTML = 'Add';
+			buttonState(Buttons, false);
+		}
+	}, [Text]);
+
+	const handleCancel = () => {
+		SetText('');
+		SetMenu(false);
+	};
+
+	// TODO usar el hook usePostTask aqui y refrescar el query "getTasks"
+	const handleOK = () => {
+		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+		Text === '' ? SetMenu(false) : SetText('');
+	};
+
+	return (
+		<div className="flex justify-between border-t p-1 shadow-lg">
+			<div className="flex">
+				<TaskMenuButton className="mr-8 ">
+					{maximizeIcon}
+					<span className="hidden xl:flex">Open</span>
+				</TaskMenuButton>
+				<TaskMenuButton Ref={todayButtonRef}>
+					{calendarIcon}
+					<span className="hidden xl:flex">Today</span>
+				</TaskMenuButton>
+				<TaskMenuButton Ref={publicButtonRef}>
+					{unlockIcon}
+					<span className="hidden xl:flex">Public</span>
+				</TaskMenuButton>
+				<TaskMenuButton Ref={normalButtonRef}>
+					{discIcon}
+					<span className="hidden xl:flex">Normal</span>
+				</TaskMenuButton>
+				<TaskMenuButton Ref={estimationButtonRef}>
+					{loaderIcon}
+					<span className="hidden xl:flex">Estimation</span>
+				</TaskMenuButton>
+			</div>
+			<div className="flex">
+				<TaskMenuButton onClick={handleCancel} className="xl:flex hidden">
+					Cancel
+				</TaskMenuButton>
+				<TaskMenuButton
+					className="text-white !bg-[#0d54ce] !px-4"
+					onClick={handleOK}
+					Ref={buttonOkRef}
+				>
+					Ok
+				</TaskMenuButton>
+			</div>
+		</div>
+	);
+}
+//! END COMPONENT
+
+export default TaskMenu;
---
+++ src/contexts/autentification.tsx
@@ -0,0 +1,27 @@
+import {
+	createContext,
+	Dispatch,
+	ReactNode,
+	SetStateAction,
+	useMemo,
+	useState,
+} from 'react';
+
+type ContextDefault = {
+	userId: string | null;
+	setUserId: Dispatch<SetStateAction<string | null>>;
+};
+
+const defaultContext = {
+	userId: null,
+	setUserId: () => {},
+} as ContextDefault;
+
+export const UserContext = createContext(defaultContext);
+
+export default function UserProvider({ children }: { children: ReactNode }) {
+	const [userId, setUserId] = useState<string | null>(null);
+	const user = useMemo(() => ({ userId, setUserId }), [userId]);
+
+	return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+}
